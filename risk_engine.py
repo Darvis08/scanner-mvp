98029a8048b3107fd5779491546ef26b9680f9af
Darvis08 <darvis.techno@gmail.com>
2025-04-22 13:12:03 +0530

    Create risk_engine.py

---
+++ risk_engine.py
@@ -0,0 +1,20 @@
+# risk_engine.py
+rules = {
+    "CFO_EXIT": 10,
+    "AUDITOR_EXIT": 10,
+    "PLEDGE_SPIKE": 8,
+    "NCLT_ADMISSION": 10,
+    "STATUTORY_DEFAULT": 9
+}
+
+def calculate_riskscore(events):
+    score = sum(rules.get(event, 0) for event in events)
+    if score >= 80:
+        band = "Severe Red"
+    elif score >= 70:
+        band = "Red"
+    elif score >= 40:
+        band = "Amber"
+    else:
+        band = "Green"
+    return score, band
